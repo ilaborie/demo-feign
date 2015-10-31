73db331fd83b6f23db789cf91988d777db90f5a9
Igor Laborie <ilaborie@gmail.com>
2015-10-31 13:35:39 +0100

    Step 3 - Param
    Add api findById
    Show @Param
    Update test

--- client/src/main/java/org/ilaborie/jub/feign/client/CatClient.java
+++ client/src/main/java/org/ilaborie/jub/feign/client/CatClient.java
@@ -1,8 +1,9 @@
 package org.ilaborie.jub.feign.client;
 
-import feign.RequestLine;
-
 import java.util.List;
+
+import feign.Param;
+import feign.RequestLine;
 
 /**  */
 public interface CatClient {
@@ -10,7 +11,9 @@ public interface CatClient {
     @RequestLine("GET /cat")
     List<Cat> findAll();
 
-//    Cat findById(String id);
+    @RequestLine("GET /cat/{id}")
+    Cat findById(@Param("id") String id);
+    
 //    Cat create(Cat cat);
 //    Cat update(String id, Cat cat);
 //    Cat delete(String id);
