9f34faf73eea789b900b7a80df4b22aa1d22d80c
Laurent Baresse <laurent.baresse@gmail.com>
2015-11-06 16:11:01 +0100

    Routes should not depend on ResponseTransformator. Otherwise how to deal with multi media type handlers ? ;-)

--- server/src/main/java/org/ilaborie/jug/feign/server/Main.java
+++ server/src/main/java/org/ilaborie/jug/feign/server/Main.java
@@ -23,23 +23,23 @@ import com.google.gson.Gson;
 import com.google.gson.GsonBuilder;
 
 public class Main {
-    
-    private static final Logger LOGGER = LoggerFactory.getLogger(Main.class); 
+
+    private static final Logger LOGGER = LoggerFactory.getLogger(Main.class);
     private static final CatService SERVICE = new CatService();
     //private static final String JSON_MIME_TYPE = "application/json; charset=utf-8";
     private static final ResponseTransformer JSON_TRANSFORMER = new JsonTransformer();
 
     public static void main(String[] args) {
         port(8000);
-        
+
         before(Main::logRequest);
-        
-        get("/cat",Main::findAllCats, JSON_TRANSFORMER);
-        get("/cat/:id", Main::findById, JSON_TRANSFORMER);
-        post("/cat", Main::createCat, JSON_TRANSFORMER);
-        put("/cat/:id", Main::updateCat, JSON_TRANSFORMER);
-        delete("/cat/:id", Main::deleteCat, JSON_TRANSFORMER);
-        
+
+        get("/cat",Main::findAllCats);
+        get("/cat/:id", Main::findById);
+        post("/cat", Main::createCat);
+        put("/cat/:id", Main::updateCat);
+        delete("/cat/:id", Main::deleteCat);
+
         exception(IllegalArgumentException.class, (e, request, response) -> error(response, 400, e));
         exception(NoSuchElementException.class, (e, request, response) -> error(response, 404, e));
     }
@@ -47,44 +47,44 @@ public class Main {
     private static void logRequest(Request request, Response response) {
         LOGGER.info("{} {}", request.requestMethod(), request.uri());
     }
-    
-    private static Collection<Cat> findAllCats(Request request, Response response) {
-        return SERVICE.findAll();
+
+    private static String findAllCats(Request request, Response response) throws Exception{
+        return JSON_TRANSFORMER.render(SERVICE.findAll());
     }
-    private static Cat findById(Request request, Response response) {
+    private static String findById(Request request, Response response) throws Exception{
         String id = request.params(":id");
         Optional<Cat> catOptional = SERVICE.findById(id);
         if (catOptional.isPresent()) {
-            return catOptional.get();
+            return JSON_TRANSFORMER.render(catOptional.get());
         }
         response.status(404);
         return null;
     }
-    private static Cat createCat(Request request, Response response) {
+    private static String createCat(Request request, Response response) throws Exception{
         Cat cat = parseCat(request.bodyAsBytes());
-        return  SERVICE.create(cat.getName(), cat.getRace());
+        return JSON_TRANSFORMER.render(SERVICE.create(cat.getName(), cat.getRace()));
     }
-    private static Cat updateCat(Request request, Response response) {
-        
+    private static String updateCat(Request request, Response response) throws Exception{
+
         String id = request.params(":id");
         Cat cat = parseCat(request.bodyAsBytes());
-        return SERVICE.update(id, cat);
+        return JSON_TRANSFORMER.render(SERVICE.update(id, cat));
     }
 
-    private static Cat deleteCat(Request request, Response response) {
+    private static String deleteCat(Request request, Response response) throws Exception {
         String userAgent = request.headers("Role");
         if (userAgent==null) {
             throw new IllegalArgumentException("Require a Role");
         }
         String id = request.params(":id");
-        return SERVICE.delete(id);
+        return JSON_TRANSFORMER.render(SERVICE.delete(id));
     }
 
     private static Cat parseCat(byte[] bytes) {
         Gson gson = new GsonBuilder().create();
         try (
-            ByteArrayInputStream input = new ByteArrayInputStream(bytes);
-            Reader reader = new InputStreamReader(input)) {
+                ByteArrayInputStream input = new ByteArrayInputStream(bytes);
+                Reader reader = new InputStreamReader(input)) {
             return gson.fromJson(reader, Cat.class);
         } catch (IOException e) {
             throw new IllegalArgumentException();
